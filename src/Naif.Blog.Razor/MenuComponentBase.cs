bfdcb6d171472c5292c0c9aa961fcd25af492c04
Charles Nurse <charlesnurse@gmail.com>
2021-04-11 14:25:59 -0700

    Initial Checkin

---
+++ src/Naif.Blog.Razor/MenuComponentBase.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Components;
+using Naif.Blog.Models;
+
+namespace Naif.Blog.Razor
+{
+    public class MenuComponentBase : ComponentBase
+    {
+        protected MenuItem CreateMenuItem(Post post)
+        {
+            MenuItem item = null;
+            switch (post.PostType)
+            {
+                case PostType.Post:
+                    item = new MenuItem
+                    {
+                        IsActive = false,
+                        Link = $"/post/{post.Slug}",
+                        Text = post.Title
+                    };
+                    break;
+                case PostType.Page:
+                    item = new MenuItem
+                    {
+                        IsActive = false,
+                        Link = $"/page/{post.Slug}",
+                        Text = post.Title
+                    };
+                    break;
+                case PostType.Blog:
+                    item = new MenuItem
+                    {
+                        Link = $"/page/blog/{post.PostTypeDetail}",
+                        IsActive = false,
+                        Text = post.Title
+                    };
+                    break;
+                case PostType.Category:
+                    item = new MenuItem
+                    {
+                        Link = $"/page/category/{post.PostTypeDetail}",
+                        IsActive = false,
+                        Text = post.Title
+                    };
+                    break;
+                case PostType.Tag:
+                    item = new MenuItem
+                    {
+                        Link = $"/page/tag/{post.PostTypeDetail}",
+                        IsActive = false,
+                        Text = post.Title
+                    };
+                    break;
+                case PostType.Url:
+                    item = new MenuItem
+                    {
+                        Link = $"{post.PostTypeDetail}",
+                        IsActive = false,
+                        Text = post.Title
+                    };
+                    break;
+            }
+
+            return item;
+        }
+
+    }
+}
